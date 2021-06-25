cf8a68498ff70ff12f6dd72e1d7ab3f719bc1f06
R4v3nG <43517528+R4v3nG@users.noreply.github.com>
2021-06-25 10:46:45 +0300

    Update mac-changer.py

--- mac-changer.py
+++ mac-changer.py
@@ -23,24 +23,33 @@ def change_mac(interface, new_mac):
 
 options = get_arguments()
 
-try:
-    with open('/sys/class/net/' + options.interface + '/address') as f:
-        sys_output = f.readlines()
-        for i in sys_output:
-            sys_output = i
-        print("[*] Current MAC: " + sys_output.strip())
-except:
-    print("[!] No such interface.")
+def get_current_mac():
+    try:
+        with open('/sys/class/net/' + options.interface + '/address') as f:
+            sys_output = f.readlines()
+            for i in sys_output:
+                sys_output = i.strip()
+        return sys_output
+    except:
+        print("[!] No such interface.")
+
+if get_current_mac():
+    print("[*] Current MAC address: " + get_current_mac())
+else:
     exit()
 
 change_mac(options.interface, options.new_mac)
 
-with open('/sys/class/net/' + options.interface + '/address') as f:
-    sys_output = f.readlines()
-    for i in sys_output:
-        sys_output = i
+def check_updated():
+    with open('/sys/class/net/' + options.interface + '/address') as f:
+        sys_output = f.readlines()
+        for i in sys_output:
+            sys_output = i.strip()
 
-if sys_output.strip() == options.new_mac:
+    return sys_output
+
+if check_updated() == options.new_mac:
     print("[+] MAC address changed to " + options.new_mac)
 else:
     print("[!] Unable to change MAC address.")
+    exit()
